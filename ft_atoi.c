8740aaf1507dfcabaeca316c9b72b95e01bba181
Sabine VITULLI <savitull@e2r1p3.42.fr>
2020-06-17 14:39:44 +0200

    testing atoi w/ static indide

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,21 @@
 /*   By: savitull <savitull@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/15 14:06:50 by savitull          #+#    #+#             */
-/*   Updated: 2020/06/16 10:30:10 by savitull         ###   ########.fr       */
+/*   Updated: 2020/06/17 14:36:55 by savitull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+static	int	ft_isspace(int c)
+{
+	if ( c == '\t' || c == '\n' || c == '\r' || c == '\v'
+		|| c == '\f' || c == 32)
+		return(1);
+	return(0);
+}
+
+int			ft_atoi(const char *str)
 {
 	unsigned int	res;
 	int 			neg;
@@ -22,7 +30,10 @@ int		ft_atoi(const char *str)
 	while (ft_isspace(*str))
 		str++;
 	if (*str == '+' || *str == '-')
-		neg = (str == '-') ? -1 : 1;
+	{
+		neg = (*str == '-') ? -1 : 1;
+		str++;
+	}
 	while(str && ft_isdigit(*str))
 	{
 		res = (res * 10) +(*str - '0');

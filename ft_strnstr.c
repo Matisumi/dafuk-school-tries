5f30df6e44bd6f8a47a72a927bc5f052bc7244ef
Sabine VITULLI <savitull@e2r9p15.42.fr>
2020-06-23 13:23:38 +0200

    strnstr works

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,13 +6,25 @@
 /*   By: savitull <savitull@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/15 14:06:29 by savitull          #+#    #+#             */
-/*   Updated: 2020/06/22 19:07:05 by savitull         ###   ########.fr       */
+/*   Updated: 2020/06/23 13:21:56 by savitull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char 	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+static int	ft_aneedle(const char *hay, const char *needle, size_t len)
+{
+	size_t i;
+
+	i = 0;
+
+	while (needle[i] && hay[i] == needle[i] && i < len)
+		i++;
+	if(needle[i] == 0)
+		return(1);
+	return(0);
+}
+char		*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	char 	*hay;
@@ -21,14 +33,12 @@ char 	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	hay = (char *)haystack;
 	if (needle[0] == 0)
 		return(hay);
-	while (hay[i])
+	while (hay[i] && i < len)
 	{
-		if (ft_strncmp(&hay[i], needle, len) == 0)
-			return(&hay[i]);
+		if (hay[i] == needle[0])
+			if (ft_aneedle(&hay[i], needle, (len - i)))
+				return(&hay[i]);
 		i++;
 	}
-	if(needle == NULL || len == 0)
-		return(hay);
-
 	return (NULL);
 }

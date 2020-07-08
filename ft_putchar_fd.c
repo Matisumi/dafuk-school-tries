5f9e67f00b74b65f1e4001d4ec8901bf015d2cf2
Sabine VITULLI <savitull@e2r11p15.42.fr>
2020-07-08 18:38:01 +0200

    update info for putchar keep looking

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: savitull <savitull@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/15 14:31:17 by savitull          #+#    #+#             */
-/*   Updated: 2020/07/07 17:56:03 by savitull         ###   ########.fr       */
+/*   Updated: 2020/07/08 18:36:41 by savitull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 
 void	ft_putchar_fd(char c, int fd)
 {
-		if((int)c > 0xff)
+	/*un char peut contenir 4 octets 
+	mask les differents octets pour les ranger dans un tableau
+	rendre ce tableau a write en lui precisant cb d'octet il doit imprimer
+	------------------
+	se renseigner sur le masking*/
+
+		/*if((int)c > 0xff)
 			write(fd, &c, sizeof(c));
-		else
+		else*/
 			write(fd, &c, 1);
 }

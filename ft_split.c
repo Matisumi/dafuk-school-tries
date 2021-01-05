12b9647e167a57b1798316b5ab9e776695ef4bb0
Sabine VITULLI <savitull@e1r10p2.42.fr>
2021-01-05 17:54:43 +0100

    split is a mess

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: savitull <savitull@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/15 14:29:27 by savitull          #+#    #+#             */
-/*   Updated: 2020/07/09 16:24:30 by savitull         ###   ########.fr       */
+/*   Updated: 2021/01/05 17:50:37 by savitull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ static int	ft_count(char const *s, char c)
 	return (tot);
 }
 
-static void	ft_freetab(char **tab)
+/*static void	ft_freetab(char **tab)
 {
 	int i;
 
@@ -67,6 +67,76 @@ char		**ft_split(char const *s, char c)
 			i++;
 	}
 	if (tab)
+		tab[x] = 0;
+	return (tab);
+}*/
+
+/*char **ft_split(char const *s, char c)
+{
+unsigned int i;
+int x;
+char **tab;
+int word_start = 0;
+
+i = 0;
+if (!s || !(tab = (char**)malloc(sizeof(char *) * ft_count(s, c) + 1)))
+    return (NULL);
+
+x = 0;
+
+    // sauter les séparateur en début de ligne (au cas ou)
+while( s[i] == c && s[i]!= 0 )
+    i++;
+
+word_start = i;
+
+while( s[i++] ) {    // teste le caractère pointé puis incrémente
+    if( s[i] == c || s[i] == 0 ) { // si séparateur trouvé ou fin de chaine
+
+                //alloue et recopie texte
+        tab[x++] = ft_substr(s, word_start, i - word_start);
+
+                // saute encore multiples séparateurs
+        while( s[i] != 0 && s[i] == c )
+            i++;
+                // si fin de chaine atteint, on quitte
+        if( s[i] == 0 ) break;
+
+        word_start = i;
+    }
+}
+
+//if (tab)
+    tab[x] = 0;
+return (tab);
+}*/
+
+char			**ft_split(char const *s, char c)
+{
+	char		**tab;
+	size_t		i;
+	size_t		x;
+	size_t		start;
+
+	i = 0;
+	x = 0;
+	if (!s || !(tab = (char **)malloc(sizeof(char*) * (ft_count(s, c) + 1))))
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] != c && (i == 0 || (s[i - 1] == c)))
+		{
+			start = i;
+			while (s[i] && s[i] != c)
+				i++;
+			if (!(tab[x] = ft_substr(s, start, (i - start))))
+				break;
+			x++;
+		}
+		else
+			i++;
+	}
+	if (tab != NULL)
 		tab[x] = 0;
 	return (tab);
 }
